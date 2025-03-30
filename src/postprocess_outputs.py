c4ebe7fb4f7d3c507c51e6bc1976544ab3bb4c17
aswathyajith <aswathy@uchicago.edu>
2025-03-29 22:06:31 -0500

    updating postprocess config generation

--- src/postprocess_outputs.py
+++ src/postprocess_outputs.py
@@ -7,18 +7,14 @@ from tqdm import tqdm
 import os
 tqdm.pandas()
 
-
 # Function to extract context length from df 
 def context_processing(x):
     context_ids, token = x["in_token_ids"], x["curr_token_id"]
     context = ast.literal_eval(re.split(r'tensor\(|\].*', context_ids)[1] + ']')
-    x["context_len"] = len(context)
+    # x["context_len"] = len(context)
     x["token_in_context"] = int(token in context)
-    x["uniq_ctxt_tkns_count"] = len(set(context))
+    # x["uniq_ctxt_tkns_count"] = len(set(context))
     return x
-    
-def get_context(x):
-    return ast.literal_eval(re.split(r'tensor\(|\].*', x)[1] + ']')
 
 def process_df(inp_path, save_path): 
     df = pd.read_csv(inp_path)
@@ -27,34 +23,25 @@ def process_df(inp_path, save_path):
     
     return df
 
-if __name__ == "__main__": 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--data_merge_config_path", type=str, default="configs/merge_config.json")
-    parser.add_argument("--output_dir", type=str, default="data/output_token_info/pythia-1.4b")
-    parser.add_argument("--data_dir", type=str, default="legal/cuad/train/sample_all")
-    parser.add_argument("--seed", type=int, default=1)
-
-    args = parser.parse_args()
-    data_merge_config_path = args.data_merge_config_path
-    data_dir = args.data_dir
-    seed = args.seed
-    output_data_dir = "/".join(data_dir.split("/")[:2])
-    output_dir = os.path.join(args.output_dir, output_data_dir, f"seed_{args.seed}")
-    
-
-    with open(data_merge_config_path, "r") as f: 
+def postprocess(postprocess_config, data_dir, seed, output_dir):
+    with open(postprocess_config, "r") as f: 
         model_map = json.load(f)
 
+    if data_dir not in model_map: 
+        raise ValueError(f"Data directory {data_dir} not found in postprocess config")
     dataset_model = model_map[data_dir]
 
     os.makedirs(output_dir, exist_ok=True)
     for model, model_info in dataset_model.items(): 
         dataset_path = os.path.join(output_dir, model+"_processed.csv")
+        print(dataset_path, flush=True)
         if os.path.exists(dataset_path): 
             print(f"Skipping {model} as it is already processed")
             continue
 
         path = model_info["path"]
+        if not os.path.exists(path): 
+            raise ValueError(f"Path {path} does not exist")
         if model != "base": 
             path = os.path.join(path, f"seed_{seed}", "tkn_freq_probs.csv")
         if "processed_path" not in model_info: 
@@ -67,7 +54,7 @@ if __name__ == "__main__":
             df = process_df(path, processed_path)
         
         # Merge with pretraining data 
-        pretraining_data = pd.read_csv("results/token_freqs/pretraining_corpus/tkn_freq.csv")
+        pretraining_data = pd.read_csv("results/token_freqs/pretrain/tkn_freq.csv")
         pretraining_data.columns = ["curr_token_id", "pt_curr_token_freq"]
         df = pd.merge(df, pretraining_data, on="curr_token_id", how="left")
         df["pt_curr_token_freq"] = df["pt_curr_token_freq"].fillna(0) # impute missing values with 0 
@@ -78,16 +65,29 @@ if __name__ == "__main__":
         pt_tot_count = df["pt_curr_token_freq"].sum()
         df["rel_prev"] = (df["curr_token_freq"] / ft_tot_count) - df["pt_curr_token_freq"] / pt_tot_count
 
-        sel_cols = ["seq_id", "context_len", "in_tokens", "curr_token", "prev_token", "curr_token_freq", "pt_curr_token_freq", "prev_token_freq", "token_in_context", "uniq_ctxt_tkns_count", "curr_token_prob", "curr_token_rank", "top_k_pred_tokens", "top_k_pred_probs", "rel_prev"]
+        sel_cols = ["seq_id", "in_tokens", "curr_token", "prev_token", "curr_token_freq", "pt_curr_token_freq", "prev_token_freq", "token_in_context","curr_token_prob", "curr_token_rank", "top_k_pred_tokens", "top_k_pred_probs", "rel_prev"]
 
         df = df[sel_cols]
-        
-        dataset_path = os.path.join(output_dir, model+"_processed.csv")
+        print("Saving dataset to ", dataset_path)
         df.to_csv(dataset_path, index=False)
-        print(df.columns)
 
     # merge_cols = ["seq_id", "context_len", "in_tokens", "curr_token", "prev_token", "curr_token_freq", "prev_token_freq", "token_in_context", "uniq_ctxt_tkns_count"]
     # combined_df = pd.merge(model_map["lora"]["df"], model_map["full"]["df"], on=merge_cols)
     # combined_df = pd.merge(combined_df, model_map["base"]["df"], on=merge_cols)
     # combined_df.to_csv(output_path, index=False)
+    
+if __name__ == "__main__": 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--postprocess_config", type=str, default="configs/postprocess_configs/pythia-1.4b.json")
+    parser.add_argument("--output_dir", type=str, default="data/output_token_info/pythia-1.4b/packing/perturbations/none")
+    parser.add_argument("--data_dir", type=str, default="wiki/wikitext/train/num_train_4096/max_seq_len_256/sample_2048")
+    parser.add_argument("--seed", type=int, default=1)
+
+    args = parser.parse_args()
+    postprocess_config = args.postprocess_config
+    data_dir = args.data_dir
+    seed = args.seed
+    output_dir = os.path.join(args.output_dir, data_dir, f"seed_{args.seed}")
+
+    postprocess(postprocess_config, data_dir, seed, output_dir)
     
