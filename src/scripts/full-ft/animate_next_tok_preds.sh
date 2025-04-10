acdf8bdbfb5daacd2883eeebea323b8c90447b9d
aswathyajith <aswathy@uchicago.edu>
2025-04-09 19:24:57 -0500

    Code Re-factoring

--- src/scripts/full-ft/animate_next_tok_preds.sh
+++
@@ -1,5 +0,0 @@
-for i in {1..5};
-    do 
-        echo Example $i;
-        python src/animate_next_token_probs.py --path_to_probs results/pythia-1.4b/full-ft/lr_2e-6/early_stopping/val/bsize_128/next_token_probs/ex_$i.json --animation_save_path "results/plots/pythia-1.4b/full-ft/lr_2e-6/early_stopping/val/ex_$i.gif"
-    done
--- src/scripts/lora/animate_next_tok_preds.sh
+++
@@ -1,6 +0,0 @@
-for i in {1..5};
-    do 
-        echo $i;
-        python src/animate_next_token_probs.py --path_to_probs results/pythia-1.4b/lora/r_16/lr_2e-4/early_stopping/val/bsize_128/ex_$i.json --animation_save_path results/plots/pythia-1.4b/lora/lr_2e-4/early_stopping/val/ex_$i.gif
-    done
-
