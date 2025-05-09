862d68e1b5cddc4bf9a179d1d76a27ad93b9f829
aswathyajith <aswathy@uchicago.edu>
2025-05-09 11:13:24 -0500

    Adding target module option

--- scripts/lora_finetuning.sh
+++ scripts/lora_finetuning.sh
@@ -4,10 +4,12 @@ source /net/projects/clab/aswathy/projects/lora_mem/scripts/env_setup.sh
 
 model_sizes=('1.4b')
 domain=$DOMAIN
-lrs=('2e-7' '2e-6' '2e-5')
-n_train_tkns=('2e4' '2e5' '2e6')
-seeds=(1 2 3)
-lora_ranks=(16)
+lrs=('2e-4' '2e-3' '2e-2')
+lrs=('2e-4')
+n_train_tkns=('2e6')
+seeds=(1)
+lora_ranks=(2048 1024 512 256 128 64 32)
+target_modules='all-linear' # "attn_only"
 
 if [ -z "${STDOUT_PATH}" ]; then
     STDOUT_PATH="logs"
@@ -28,9 +30,9 @@ do
                     
                     stdout_file="$STDOUT_PATH/log_domain-${domain}_model-${model_size}_lora-r${lora_rank}_lr-${lr}_n_train_tkns-${n_tkns}_seed${seed}.out"
 
-                    echo "python src/training/finetune_model.py --base_model EleutherAI/pythia-$model_size --model_config_path configs/model_configs/lora/pythia-${model_size}.json --sft_config_path configs/model_configs/sft_configs.json --lora_config_path configs/lora_config.json  --packing --seed $seed --domain $domain --lr $lr --lora --lora_rank $lora_rank --dataset_config configs/dataset_config.json --resume_from_checkpoint --n_train_tkns $n_tkns >> $stdout_file 2>&1"
+                    echo "python src/training/finetune_model.py --base_model EleutherAI/pythia-$model_size --model_config_path configs/model_configs/lora/pythia-${model_size}.json --sft_config_path configs/model_configs/sft_configs.json --lora_config_path configs/lora_config.json  --packing --seed $seed --domain $domain --lr $lr --lora --lora_rank $lora_rank --dataset_config configs/dataset_config.json --resume_from_checkpoint --n_train_tkns $n_tkns --target_modules $target_modules >> $stdout_file 2>&1"
 
-                    python src/training/finetune_model.py --base_model EleutherAI/pythia-$model_size --model_config_path configs/model_configs/lora/pythia-${model_size}.json --sft_config_path configs/model_configs/sft_configs.json --lora_config_path configs/lora_config.json  --packing --seed $seed --domain $domain --lr $lr --lora --lora_rank $lora_rank --dataset_config configs/dataset_config.json --resume_from_checkpoint --n_train_tkns $n_tkns >> $stdout_file 2>&1  
+                    python src/training/finetune_model.py --base_model EleutherAI/pythia-$model_size --model_config_path configs/model_configs/lora/pythia-${model_size}.json --sft_config_path configs/model_configs/sft_configs.json --lora_config_path configs/lora_config.json  --packing --seed $seed --domain $domain --lr $lr --lora --lora_rank $lora_rank --dataset_config configs/dataset_config.json --resume_from_checkpoint --n_train_tkns $n_tkns --target_modules $target_modules >> $stdout_file 2>&1  
 
                     echo "Exit code: $?"
                 done
